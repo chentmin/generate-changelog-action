fedb1d0bc524c533db1e568a97fdfa8d3310841c
Timmy <chentmin@users.noreply.github.com>
2022-05-03 22:04:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ if [ -z "$PACKAGE_DIR" ]; then
   echo "No path for the package.json passed. Fallbacking to root directory."
 else
   echo "package-dir detected. Using its value."
-  cp "$PACKAGE_DIR" package.json
+  cd "$PACKAGE_DIR"
 fi
 
 if [ -z "$FROM_TAG" ]; then
